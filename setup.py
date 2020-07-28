ffc5b72f9c28c1e29a74995ecb1aaca026e8f465
andersh <anders.haal@ingby.com>
2020-07-28 16:08:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+  
+setup(
+    name='sample',
+    version='0.1.1',
+    packages=find_packages(),
+    author='thenodon',
+    author_email='anders@opsdis.com',
+    url='https://github.com/opsdis/sample',
+    license='GPLv3',
+    include_package_data=True,
+    zip_safe=False,
+)
+
