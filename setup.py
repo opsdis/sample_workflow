be43fd1f0054f5037ea77dbbb9c7fa867677e534
andersh <anders.haal@ingby.com>
2020-07-28 23:48:11 +0200

    testing with encrypted key

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from setuptools import find_packages, setup
-  
+
+
+
+#from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import setup, find_packages
+
+from manage_version import get_version
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
+
 setup(
-    name='sample',
-    version='0.1.1',
+    name='hello',
+    version=get_version(),
     packages=find_packages(),
     author='thenodon',
     author_email='anders@opsdis.com',
-    url='https://github.com/opsdis/sample',
+    url='https://github.com/opsdis/sample_workflow',
     license='GPLv3',
     include_package_data=True,
     zip_safe=False,
+    description='HelloWorld',
+    install_requires=read('requirements.txt').split(),
+    python_requires='>=3.6',
 )
-
